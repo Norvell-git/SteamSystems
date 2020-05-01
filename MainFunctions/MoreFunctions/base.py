d4de629e1e93fa0c3e5f0cf3dfae4d40c8ba2edf
Norvell-git <64608160+Norvell-git@users.noreply.github.com>
2020-05-01 10:03:09 +0100

    Add files via upload

---
+++ MainFunctions/MoreFunctions/base.py
@@ -0,0 +1,33 @@
+"""
+Basic functions required for turbine models
+"""
+
+def abc(P_1, P_2):
+	"""
+	Modelling parameters from Sun and Smith 2015
+	"""
+	if P_2 < 1.013:
+	    a = 1.3150    - 1.6347e-3 * P_1 - 0.36798   * P_2
+	    b = - 437.77  + 29.007    * P_1 + 10.359    * P_2
+	    c = 7.8863e-3 + 5.2833e-4 * P_1 - 0.70315   * P_2
+	else:
+	    a = 1.1880    - 2.9564e-4 * P_1 + 4.6473e-3 * P_2
+	    b = 449.98    + 5.6702    * P_1 - 11.505    * P_2
+	    c = 0.20515   - 6.9517e-4 * P_1 + 2.8446e-3 * P_2
+	return [a,b,c]
+
+def dh_IS(inlet, outlet_IS):
+	return inlet.h - outlet_IS.h
+
+def n(a, b, c, dh_IS, m_max):
+	return (1+c)/a * (dh_IS - b/m_max)
+
+def Wint(a, b, c, dh_IS, m_max):
+	return (c/a)*(m_max*dh_IS - b)
+
+def W(m_act, n, Wint):
+	return m_act * n - Wint
+
+def h_2(inlet, W, eff_mech, m_act):
+	return inlet.h - W / (eff_mech * m_act)
+
