be74943823755a318cfabaf3e6d8067e29bb7d3d
Norvell-git <64608160+Norvell-git@users.noreply.github.com>
2020-05-04 23:47:22 +0100

    Add files via upload

--- MainFunctions/simplespec.py
+++ MainFunctions/simplespec.py
@@ -1,6 +1,6 @@
 """
 Simple function to find outlet conditions of a steam turbine
-	for a fully spec'd turbine
+    for a fully spec'd turbine
 
 Input: m_act, m_max
 
@@ -14,17 +14,18 @@ from MainFunctions.MoreFunctions import base
 
 def simple(inlet, outlet_IS, m_act, m_max, eff_mech):
 
-	a = base.abc(inlet.P, outlet_IS.P)[0]
-	b = base.abc(inlet.P, outlet_IS.P)[1]
-	c = base.abc(inlet.P, outlet_IS.P)[2]
+    abc = base.abc(inlet.P*10, outlet_IS.P*10)  # Output in bara
+    a = abc[0]
+    b = abc[1]
+    c = abc[2]
 
-	dh_IS = base.dh_IS(inlet, outlet_IS)
+    dh_IS = base.dh_IS(inlet, outlet_IS)
 
-	n = base.n(a, b, c, dh_IS, m_max)
-	Wint = base.Wint(a, b, c, dh_IS, m_max)
+    n = base.n(a, b, c, dh_IS, m_max)
+    Wint = base.Wint(a, b, c, dh_IS, m_max)
 
-	W = base.W(m_act, n, Wint)
+    W = base.W(m_act, n, Wint)
 
-	h_2 = base.h_2(inlet, W, eff_mech, m_act)
+    h_2 = base.h_2(inlet, W, eff_mech, m_act)
 
-	return IAPWS97(P = (outlet_IS.P/10), h = h_2)
+    return IAPWS97(P=(outlet_IS.P/10), h=h_2)
