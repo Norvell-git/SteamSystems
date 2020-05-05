240f67bcb8b29a85288b725ff0597b72d964d16e
Norvell-git <64608160+Norvell-git@users.noreply.github.com>
2020-05-05 23:34:12 +0100

    Update __init__.py

--- turbines/functions/__init__.py
+++ turbines/functions/__init__.py
@@ -1,5 +1,5 @@
 """
-__init__ file for MainFunctions
+__init__ file for functions
 """
 
 from .simplespec import simple
@@ -9,3 +9,16 @@ from .heatspec import heatspec
 from .workspec import workspec
 
 from .spec_exceptions import non_specd
+
+from .base import f_abc
+from .base import f_dh_IS
+from .base import f_n
+from .base import f_Wint
+from .base import f_W
+from .base import f_h_2
+
+from .errors import OverSpecififedError
+from .errors import NonSpecifiedError
+from .errors import MaxIterationsError
+from .errors import InputError
+from .errors import OutputError
