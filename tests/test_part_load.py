d0f14e0b2bf754a3f6785798872705ae308636ca
Norvell-git <64608160+Norvell-git@users.noreply.github.com>
2020-05-06 00:31:32 +0100

    Update test_part_load.py

--- tests/test_part_load.py
+++ tests/test_part_load.py
@@ -22,7 +22,7 @@ Tests should be accurate within a relative tolerance of 1% for variables:
 
 from pytest import approx
 
-from turbine import Turbine
+from SteamSystems import Turbine
 
 
 # Case 2 - Part Load (t):
